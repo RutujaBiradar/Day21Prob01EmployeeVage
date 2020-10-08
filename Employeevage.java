693fe8257e60d15e0a6d434f86bd7dc7321d3243
RutujaBiradar <rutujabiradar596@gmail.com>
2020-10-08 00:14:50 -0400

    UserCase1 Added

---
+++ Employeevage.java
@@ -0,0 +1,11 @@
+public class EmployeeVage {
+	public static void main(String args[]) {
+	  int IsFullTime = 1;
+	  double empCheck = Math.floor(Math.random() * 10) % 2;
+	if (empCheck == IsFullTime)
+	   System.out.println("Employee is present");
+	else
+	    System.out.println("Employee is not present");
+     }
+}
+
